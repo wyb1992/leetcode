fa7e6b55626b441e048f472432ae8b2a6a1d8f68
wyb <wyb496926024@gmail.com>
2018-08-21 18:11:49 +0800

    Valid Parentheses

---
+++ src/main/java/com/example/leetcode/validParentheses/ValidParentheses.java
@@ -0,0 +1,58 @@
+package com.example.leetcode.validParentheses;
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ * 示例 1:
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ * 输入: "{[]}"
+ * 输出: true
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        boolean result = new ValidParentheses().isValid("()");
+        System.out.println(result);
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+            if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+            if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
