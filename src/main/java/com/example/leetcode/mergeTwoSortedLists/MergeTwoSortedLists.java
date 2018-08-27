3665432d74fb801dff0cd890cf94660ea269694a
wyb <wyb496926024@gmail.com>
2018-08-27 14:08:31 +0800

    Merge Two Lists

---
+++ src/main/java/com/example/leetcode/mergeTwoSortedLists/MergeTwoSortedLists.java
@@ -0,0 +1,47 @@
+package com.example.leetcode.mergeTwoSortedLists;
+
+/**
+ * Created by wyb on 2018/8/27.
+ */
+public class MergeTwoSortedLists {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = new MergeTwoSortedLists().mergeTwoLists(l1, l2);
+        System.out.println(result);
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode lastNode = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                lastNode.next = l1;
+                l1 = l1.next;
+            } else {
+                lastNode.next = l2;
+                l2 = l2.next;
+            }
+            lastNode = lastNode.next;
+        }
+
+        lastNode.next = l1 != null ? l1 : l2;
+
+        return dummy.next;
+
+    }
+}
