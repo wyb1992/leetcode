1e01b0cb56af1a8d65035fed492af8c345bed5b7
wyb <wyb496926024@gmail.com>
2018-09-17 13:49:40 +0800

    Search Insert Position

---
+++ src/main/java/com/example/leetcode/searchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,60 @@
+package com.example.leetcode.searchInsertPosition;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 你可以假设数组中无重复元素。
+ * 示例 1:
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ * 示例 2:
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ * 示例 3:
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ * 示例 4:
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ */
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        int nums[] = {1, 3, 5, 6};
+        int target = 5;
+        int result = new SearchInsertPosition().searchInsert(nums, target);
+        System.out.println(JSONObject.toJSONString(result));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int start = 0, end = nums.length - 1;
+
+        // 二分法确定位置
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        // 比较值的大小
+        if (nums[start] >= target) {
+            return start;
+        } else if (nums[end] >= target) {
+            return end;
+        } else {
+            return end + 1;
+        }
+    }
+}
