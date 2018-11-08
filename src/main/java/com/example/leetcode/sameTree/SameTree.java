f7f213bb376bb062467df44bc5e8c15469b1ae39
wyb <wyb496926024@gmail.com>
2018-11-08 17:43:49 +0800

    Same Tree

---
+++ src/main/java/com/example/leetcode/sameTree/SameTree.java
@@ -0,0 +1,42 @@
+package com.example.leetcode.sameTree;
+
+/**
+ * Created by wyb on 2018/11/8.
+ */
+public class SameTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        boolean result = new SameTree().isSameTree(p, q);
+        System.out.println(result);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
