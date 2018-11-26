9e699568a2b89bd1c5cfd9da42b6cc8172d8e371
wyb <wyb496926024@gmail.com>
2018-11-26 16:37:36 +0800

    Binary Tree Level Order Traversal

---
+++ src/main/java/com/example/leetcode/binaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,63 @@
+package com.example.leetcode.binaryTreeLevelOrderTraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by wyb on 2018/11/26.
+ */
+public class BinaryTreeLevelOrderTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        List<List<Integer>> result = new BinaryTreeLevelOrderTraversal().levelOrderBottom(root);
+        System.out.println(result);
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List result = new ArrayList();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+}
