d48bd1bd2e915586b59669130d8b49cb2de2863d
wyb <wyb496926024@gmail.com>
2018-12-04 18:55:28 +0800

    Path Sum

---
+++ src/main/java/com/example/leetcode/pathSum/PathSum.java
@@ -0,0 +1,46 @@
+package com.example.leetcode.pathSum;
+
+/**
+ * Created by wyb on 2018/12/4.
+ */
+public class PathSum {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+
+        root.left.left = new TreeNode(11);
+
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+
+        root.right.right.right = new TreeNode(1);
+
+        boolean flag = new PathSum().hasPathSum(root, 22);
+
+        System.out.println(flag);
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum (root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
